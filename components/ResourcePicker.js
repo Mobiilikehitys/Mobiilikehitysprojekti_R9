2646c798dec28ed551ec25726fee40cdd69dad58
APJokinen <arjokine@students.oamk.fi>
2025-03-25 12:14:12 +0200

    25.3.

---
+++ components/ResourcePicker.js
@@ -0,0 +1,31 @@
+import { StyleSheet, View } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import {useEffect, useState} from 'react'
+
+export default function ResourcePicker({resources, setResource}){
+    return(
+        <View style={styles.container}>
+            <Picker style={styles.picker}
+                selectedValue={resource}
+                onValueChange={(itemValue, itemIndex) => 
+                    setResource(itemValue)
+                }
+            >
+                {resources.map((item, index) => 
+                <Picker.Item key={index} style={styles.item} label={item} value={item}/>
+                )}
+
+            </Picker>
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+    picker:{
+
+    }
+})
---
+++ components/timeToMinutes.js
@@ -0,0 +1,8 @@
+
+export default function timeToMinutes (clocktime){
+    const split = clocktime.split(".")
+    const hourString = split[0]
+    const minuteString = split[1]
+    const minutes = parseInt(hourString)*60+parseInt(minuteString)
+    return minutes
+}
