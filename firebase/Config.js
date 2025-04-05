bfc65df5d82b4ac3303ac7d4be5be7fc04404794
APJokinen <arjokine@students.oamk.fi>
2025-04-05 12:43:21 +0300

    apikey:t pois

--- firebase/Config.js
+++ firebase/Config.js
@@ -5,13 +5,7 @@ import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, createUserWith
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBx6D9a1lIqU0frIbKq0Cs8XddlXonHfsM",
-  authDomain: "project-app-with-functions.firebaseapp.com",
-  projectId: "project-app-with-functions",
-  storageBucket: "project-app-with-functions.firebasestorage.app",
-  messagingSenderId: "256966699966",
-  appId: "1:256966699966:web:b5fc166e5d81d39afe67ef",
-  measurementId: "G-XP80D5H75B"
+  
 };
 
 const app = initializeApp(firebaseConfig);
