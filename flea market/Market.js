216b5fbf3ab2bde2f318d9c56ae34b5bd387d64e
Arttu Pirilä <t3piar00@students.oamk.fi>
2025-04-23 10:29:02 +0300

    flea market & bulletin delete buttons

--- flea market/Market.js
+++ flea market/Market.js
@@ -6,34 +6,58 @@ import Icon from 'react-native-vector-icons/FontAwesome'
 import NewItemModal from "./newItemModal";
 import { convertFirebaseTimeStampJS } from "../Bulletin board/serverTimeStampToJSON";
 import deleteItem from "../firebase/Delete";
-
-export default function FleaMarket({user}){
+import { useAuth } from "../context/AuthContext";
+export default function FleaMarket(){
+    const { user } = useAuth()
     const [modalVisible, setModalVisible] = useState(false)
     const oldData = useDataOrder(collection(firestore,MARKETPRODUCTS))
 
     const RenderItem = (item) => {
-        const timeStamp = convertFirebaseTimeStampJS(item.luotu)
-        return(
+        const timeStamp = convertFirebaseTimeStampJS(item.luotu);
+    
+        return (
             <View style={styles.centerView}>
-            <View style={styles.market}>
-            <View style={styles.createdView}>
-            <Text style={styles.createdText}>{item.henkilo}{" "}{timeStamp}</Text></View>
-            {item.henkilo == user && <View style={styles.closeButton}><Pressable onPress={() => {deleteItem(MARKETPRODUCTS, item.id)}}>
-                <Icon name="times" size={30} color="#000" /></Pressable></View>}
-
-                {item.hinta == 0 || !item.hinta ? <View style={styles.giveAwayView}><Text style={styles.giveAwayText}>Annetaan</Text></View>:<View><View style={styles.headerView}>
-                <Text style={styles.headerLabel}>Myydään: </Text>
-                    <Text style={styles.headerText}>{item.tuote}</Text>
+                <View style={styles.market}>
+                    <View style={styles.createdView}>
+                        <Text style={styles.createdText}>{item.henkilo}{" "}{timeStamp}</Text>
+                    </View>
+                    {user?.accountType === "company" && (
+                        <View style={styles.closeButton}>
+                            <Pressable onPress={() => { deleteItem(MARKETPRODUCTS, item.id); }}>
+                                <Icon name="trash" size={30} color="#ff6b6b" />
+                            </Pressable>
+                        </View>
+                    )}
+                    {item.henkilo === user.email && (
+                        <View style={styles.closeButton}>
+                            <Pressable onPress={() => { deleteItem(MARKETPRODUCTS, item.id); }}>
+                                <Icon name="times" size={30} color="#000" />
+                            </Pressable>
+                        </View>
+                    )}
+                    {item.hinta == 0 || !item.hinta ? (
+                        <View style={styles.giveAwayView}>
+                            <Text style={styles.giveAwayText}>Annetaan</Text>
+                        </View>
+                    ) : (
+                        <View>
+                            <View style={styles.headerView}>
+                                <Text style={styles.headerLabel}>Myydään: </Text>
+                                <Text style={styles.headerText}>{item.tuote}</Text>
+                            </View>
+                            <View style={styles.priceView}>
+                                <Text style={styles.priceLabel}>Hinta: </Text>
+                                <Text style={styles.priceText}>{item.hinta}{"€"}</Text>
+                            </View>
+                        </View>
+                    )}
+                    <View style={styles.messageView}>
+                        <Text style={styles.messageText}>{item.viesti}</Text>
+                    </View>
                 </View>
-
-                
-                <View style={styles.priceView}>
-                <Text style={styles.priceLabel}>Hinta: </Text>
-                <Text style={styles.priceText}>{item.hinta}{"€"}</Text></View></View>}
-                <View style={styles.messageView}><Text style={styles.messageText}>{item.viesti}</Text></View>
-        </View></View>)
-    }
-
+            </View>
+        );
+    };
     return(
             <View style={styles.centerView}>
                 <View style={styles.centerView}>
